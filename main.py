aaff5380cff0185ce139cb38b3c911aa1661278a
Mbote-Joseph <mbotejoseph001@gmail.com>
2021-07-14 22:23:31 +0300

    dynamic array

--- main.py
+++ main.py
@@ -1,31 +1,58 @@
-class Car:
-    def __init__(self):
-        self.name=""
-        self.wheels=0
-        self.color=""
+# Python program to check for the sum
+# condition to be satisified
 
-    def setName(self,name):
-        self.name=name
-    
-    def getName(self):
-        return self.name
+def hasArrayTwoCandidates(A, arr_size, sum):
+	
+	# sort the array
+	quickSort(A, 0, arr_size-1)
+	l = 0
+	r = arr_size-1
+	
+	# traverse the array for the two elements
+	while l<r:
+		if (A[l] + A[r] == sum):
+			return 1
+		elif (A[l] + A[r] < sum):
+			l += 1
+		else:
+			r -= 1
+	return 0
 
-    def setWheels(self,wheels):
-        self.wheels=wheels
+# Implementation of Quick Sort
+# A[] --> Array to be sorted
+# si --> Starting index
+# ei --> Ending index
+def quickSort(A, si, ei):
+	if si < ei:
+		pi = partition(A, si, ei)
+		quickSort(A, si, pi-1)
+		quickSort(A, pi + 1, ei)
 
-    def getWheels(self):
-        return self.wheels
+# Utility function for partitioning
+# the array(used in quick sort)
+def partition(A, si, ei):
+	x = A[ei]
+	i = (si-1)
+	for j in range(si, ei):
+		if A[j] <= x:
+			i += 1
+			
+			# This operation is used to swap
+			# two variables is python
+			A[i], A[j] = A[j], A[i]
 
-    def setColor(self,color):
-        self.color=color
+		A[i + 1], A[ei] = A[ei], A[i + 1]
+		
+	return i + 1
+	
 
-    def getColor(self):
-        return self.color
+# Driver program to test the functions
+A = [1, 4, 45, 6, 10, -8]
+n = 16
+if (hasArrayTwoCandidates(A, len(A), n)):
+	print("Array has two elements with the given sum")
+else:
+	print("Array doesn't have two elements
+								with the given sum")
 
-toyota =  Car()
-toyota.setName("Toyota Jeep")
-print(toyota.getName())
-toyota.setWheels(4)
-print(toyota.getWheels())
-toyota.setColor("Red")
-print(toyota.getColor())
+## This code is contributed by __Devesh Agrawal__
